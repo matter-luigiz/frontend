fb3880b7e67f719cb05acc6854a95bf3c88a1e9f
Gabe Guralnick <gnguralnick@gmail.com>
2022-12-14 13:39:00 -0500

    feat: add basic connection to backend

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './global.scss';
 import Styles from './App.module.scss';
 import classNames from "classnames/bind";
@@ -6,11 +6,26 @@ import classNames from "classnames/bind";
 const cx = classNames.bind(Styles);
 
 function App() {
-  return (
-    <div className={cx('App')}>
-      <h1>Hello, World!</h1>
-    </div>
-  );
+    const [listing, setListing] = useState();
+    const [error, setError] = useState('');
+    const [site, setSite] = useState('');
+
+    const getListing = (site: string) => fetch(`http://${process.env.REACT_APP_BACKEND_URL}/${site}`)
+        .then(res => res.json())
+        .then(res => setListing(res))
+        .catch(() => setError(`Failed to get listing for site ${site}`));
+
+
+    return (
+        <div className={cx('App')}>
+            <form onSubmit={e => { e.preventDefault(); getListing(site).then(r => r); } }>
+                <input value={site} onChange={e => setSite(e.target.value)}/>
+                <input type={"submit"} />
+            </form>
+
+            <p>{error ? error : listing ? JSON.stringify(listing) : 'Enter a site'}</p>
+        </div>
+    );
 }
 
 export default App;
