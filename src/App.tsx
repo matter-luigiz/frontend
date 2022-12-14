1625aef8afbadcdb2474bf6e606db881a8f0d21a
Gabe Guralnick <gnguralnick@gmail.com>
2022-12-13 20:26:32 -0500

    feat: add classNames and scss

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,12 @@
 import React from 'react';
+import Styles from './App.module.scss';
+import classNames from "classnames/bind";
+
+const cx = classNames.bind(Styles);
 
 function App() {
   return (
-    <div className="App">
+    <div className={cx('App')}>
       Hello, World!
     </div>
   );
