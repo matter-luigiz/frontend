3bd960cf0d5889e1656975b32af654d244dc0a68
Gabe Guralnick <gnguralnick@gmail.com>
2022-12-28 14:23:22 -0600

    fix: implement product page

--- src/routes/ProductPage/ProductPage.tsx
+++ src/routes/ProductPage/ProductPage.tsx
@@ -3,6 +3,7 @@ import {useParams} from "react-router-dom";
 import Styles from './ProductPage.module.scss';
 import classNames from "classnames/bind";
 import Breadcrumbs from "../../components/Breadcrumbs/Breadcrumbs";
+import externalLink from '../../icons/external.svg';
 
 const cx = classNames.bind(Styles);
 
@@ -27,6 +28,21 @@ const ProductPage = () => {
     return <div className={cx('product')}>
         <div className={'page'}>
             <Breadcrumbs prev={[{text: product.category, link: `/search?cat=${product.category}`}]} current={product.name} />
+            <div className={cx('product-info')}>
+                <img src={product.imageUrl} alt={product.alt} className={cx('product-image')} />
+                <div className={cx('product-text')}>
+                    <h2>{product.name}</h2>
+                    <p>{product.category}</p>
+                    <h3>$ {product.price}/kg</h3>
+                    <div className={cx('description')}>
+                        <h4>Information on Material</h4>
+                    </div>
+                    <div className={cx('buttons')}>
+                        <button className={'primary'}>View on supplier's site <img src={externalLink} alt={''}/></button>
+                        <button className={'quaternary'}>Save for Later</button>
+                    </div>
+                </div>
+            </div>
         </div>
     </div>;
 };
