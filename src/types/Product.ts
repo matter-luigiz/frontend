81b8d7713961f0be06a7166a1f512fee3bff2871
Gabe Guralnick <gnguralnick@gmail.com>
2023-01-03 20:21:41 -0600

    feat: add function to convert objects to Products

--- src/types/Product.ts
+++ src/types/Product.ts
@@ -2,12 +2,47 @@ type Product = {
     name: string;
     category: string;
     price?: string;
+    description?: string;
     image: string;
+    link: string;
     alt: string;
-    liked: Boolean;
+    liked: boolean;
     id: Number;
     site: string;
     otherFields?: any;
 }
 
+function convertToProduct(item: any): Product {
+    let src = item[1];
+    let prod: Product = {
+        name: item[0],
+        image: src.Image.slice(6),
+        category: src.category || src.Category || src['Commodity Type'],
+        price: src['Price per tons'],
+        site: src.Site,
+        id: src.id || src['ID'],
+        link: src.Link,
+        description: src.description,
+        liked: false,
+        alt: ''
+    }
+
+    let otherFields = {...src};
+
+    delete otherFields.Image;
+    delete otherFields.Category;
+    delete otherFields['Commodity Type'];
+    delete otherFields['Price per tons'];
+    delete otherFields.Site;
+    delete otherFields.id;
+    delete otherFields['ID'];
+    delete otherFields.Link;
+    delete otherFields.description;
+
+    prod.otherFields = {...otherFields};
+
+    return prod;
+}
+
 export default Product;
+export {convertToProduct};
