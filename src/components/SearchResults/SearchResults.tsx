927e23b45029468c4635b738805e393092a04f46
Gabe Guralnick <gnguralnick@gmail.com>
2023-01-03 20:22:26 -0600

    fix: properly convert search results to products

--- src/components/SearchResults/SearchResults.tsx
+++ src/components/SearchResults/SearchResults.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import {useBackendReq} from "../../hooks";
-import Product from "../../types/Product";
+import Product, {convertToProduct} from "../../types/Product";
 import ProductGrid from "../ProductGrid/ProductGrid";
 import Styles from './SearchResults.module.scss';
 import classNames from "classnames/bind";
@@ -30,15 +30,7 @@ const SearchResults = ({search, category, page, updatePage}: SearchProps) => {
         </div>
     } else {
         const res = data as {data: any[], size: number};
-        results = (res.data).map(item =>
-            ({
-                name: item[0],
-                image: item[1].Image.slice(6),
-                category: item[1].category || item[1]['Commodity Type'],
-                price: item[1]['Price per tons'],
-                site: item[1].Site,
-                otherFields: {...item[1]}
-            })) as Product[];
+        results = (res.data).map(convertToProduct) as Product[];
         count = res.size;
     }
 
