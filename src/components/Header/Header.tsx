92964353e3df7fcfc9814cd6308c887d5800810a
Gabe Guralnick <gnguralnick@gmail.com>
2023-01-07 11:12:43 -0500

    feat: make header responsive

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,8 +1,9 @@
-import React from "react";
+import React, {useState} from "react";
 import textlogo from '../../icons/textlogo.svg';
 import Styles from './header.module.scss';
 import {Link, useNavigate} from "react-router-dom";
 import classNames from "classnames/bind";
+import menuIcon from '../../icons/menu-icon.svg';
 
 const cx = classNames.bind(Styles);
 
@@ -19,24 +20,33 @@ interface HeaderProps {
 const Header = (props: HeaderProps) => {
     const {tabs} = props;
     const navigate = useNavigate();
+    const [showNav, setShowNav] = useState(true);
 
     return (
         <header className={cx('header')}>
-            <Link to={'/'} className={cx('homelink')}><img src={textlogo} alt={'matter'}/></Link>
-            <nav className={cx('navbar')}>
-                <ul>
-                    {
-                        tabs.map(tab =>
-                            <li key={tab.link} className={cx({'selected': tab.selected})} onClick={() => navigate(tab.link)}>
-                                <Link to={tab.link}>{tab.text}</Link>
-                            </li>
-                        )
-                    }
-                </ul>
-            </nav>
-            <div className={cx('buttons')}>
-                <button className={'secondary'}>Sign in</button>
-                <button className={'primary'}>Register</button>
+            <div className={cx('mobile-top-ctr')}>
+                <Link to={'/'} className={cx('homelink')}><img src={textlogo} alt={'matter'}/></Link>
+                <button className={cx('nav-toggle')} onClick={() => setShowNav(!showNav)}>
+                    <img src={menuIcon} alt={''}/>
+                </button>
+            </div>
+            <div className={cx('nav-ctr', {'hide': !showNav})}>
+                <nav className={cx('navbar')}>
+                    <ul>
+                        {
+                            tabs.map(tab =>
+                                <li key={tab.link} className={cx({'selected': tab.selected})}
+                                    onClick={() => navigate(tab.link)}>
+                                    <Link to={tab.link}>{tab.text}</Link>
+                                </li>
+                            )
+                        }
+                    </ul>
+                </nav>
+                <div className={cx('buttons')}>
+                    <button className={'secondary'}>Sign in</button>
+                    <button className={'primary'}>Register</button>
+                </div>
             </div>
         </header>
     );
