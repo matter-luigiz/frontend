a574def6bd5962038bb8287285b8a342d1fd0edd
Gabe Guralnick <gnguralnick@gmail.com>
2023-01-12 19:26:42 -0500

    feat: add placeholder for learn page

--- src/routes/Learn/Learn.tsx
+++ src/routes/Learn/Learn.tsx
@@ -1,4 +1,9 @@
 import React, {useEffect} from "react";
+import Styles from './Learn.module.scss';
+import classNames from "classnames/bind";
+import constImg from '../../icons/const-img.svg';
+
+const cx = classNames.bind(Styles);
 
 const Learn = () => {
 
@@ -7,9 +12,12 @@ const Learn = () => {
     }, []);
 
     return (
-        <div className={'learn'}>
+        <div className={cx('learn')}>
             <div className={'page'}>
-                Welcome to Learn!
+                <div className={cx('construction')}>
+                    <img src={constImg} alt={''}/>
+                    <h2>Under construction, check back soon!</h2>
+                </div>
             </div>
         </div>
     );
